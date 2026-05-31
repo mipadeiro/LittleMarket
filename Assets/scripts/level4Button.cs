cae7e9bb8a6605e51aa667dd56403a31e99d6bae
mipadeiro <mipadeiro04@gmail.com>
2026-05-31 22:42:51 +0100

    fixed level buttons

---
+++ Assets/scripts/level4Button.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class level4Button : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene("Level4");
+    }
+}
